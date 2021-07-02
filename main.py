775f8b9c4b1f2bc1390bf59e374e249cd8a767e6
obrienb <s3298931@student.rmit.edu.au>
2021-07-02 16:13:58 +1000

    Friday arvo

--- main.py
+++ main.py
@@ -78,14 +78,62 @@ def queryOne():
 
     return results_list
 
+# function to run query 2
+def queryTwo():
+
+    # define the query
+    QUERY = (
+        'select names.string_field_1 as Country, trade_deficit_value '
+        'from ('
+        'select exports.country_code, (imports.imports - exports.exports) as trade_deficit_value '
+        'from ('
+        'select country_code, SUM(value) as exports '
+        'from `s3298931-a1-2.gsquarterlySeptember20.gsquarterlySeptember20` '
+        'where status = "F" and account = "Exports" and product_type = "Goods" and '
+        'time_ref >= 201400 and time_ref <= 201612 '
+        'group by country_code) exports '
+
+        'inner join '
+
+        '(select country_code, SUM(value) as imports '
+        'from `s3298931-a1-2.gsquarterlySeptember20.gsquarterlySeptember20` '
+        'where status = "F" and account = "Imports" and product_type = "Goods" and '
+        'time_ref >= 201400 and time_ref <= 201612 '
+        'group by country_code '
+        ') as imports '
+        'on exports.country_code = imports.country_code '
+        'limit 50 '
+        ') as primary '
+
+        'inner join '
+
+        '`s3298931-a1-2.gsquarterlySeptember20.country_classification` as names '
+        'on primary.country_code = names.string_field_0 '
+        'order by trade_deficit_value DESC'
+    )
+
+    # execute the query
+    query_job = client.query(QUERY)
+    rows = query_job.result()
+
+    # create an empty list to store the results
+    results_list = []
+
+    for e in rows:
+        results_list.append(e)
+
+    return results_list
+
 # home page
 @app.route("/")
 def home():
 
     query_one = []
     query_one = queryOne()
-    
-    return render_template("index.html", q_one=query_one)
+
+    query_two = queryTwo()
+
+    return render_template("index.html", q_one=query_one, q_two=query_two)
     
 if __name__ == "__main__":
     app.run(debug=True)
